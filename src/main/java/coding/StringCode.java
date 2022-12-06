fa6ae762875b278292f905274af321f404d1b969
NEHA <nehakonda214@mail.com>
2022-12-06 21:16:28 +0530

    output of h4j3n4

---
+++ src/main/java/coding/StringCode.java
@@ -0,0 +1,26 @@
+package coding;
+
+public class StringCode{
+    public static void main(String[] args) {
+
+        String s = "hhhhjjjnnnn";
+        int a;
+        int b;
+        int c;
+        for(int i =0; i<s.length(); i++){
+            if(s.charAt(i) == "h" ||)
+            if(s == "h" ){
+
+                a++;
+            }
+                if(s =="j") {
+                    b++;
+                }
+                    else(s== "n"){
+                        c++;
+                    }
+                }
+            }
+        }
+    }
+}
