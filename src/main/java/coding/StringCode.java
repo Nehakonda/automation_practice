413023007a11a5a781408a0402729b032551a6c3
NEHA <nehakonda214@mail.com>
2022-12-08 13:31:52 +0530

    output of h4j3n4

--- src/main/java/coding/StringCode.java
+++ src/main/java/coding/StringCode.java
@@ -4,23 +4,22 @@ public class StringCode{
     public static void main(String[] args) {
 
         String s = "hhhhjjjnnnn";
-        int a;
-        int b;
-        int c;
-        for(int i =0; i<s.length(); i++){
-            if(s.charAt(i) == "h" ||)
-            if(s == "h" ){
-
+        char[] s1 = s.toCharArray();
+        int a=0;
+        int b=0;
+        int c=0;
+        for(int i = 0; i< s.length(); i++){
+            if (s.charAt(i) == 'h'){
                 a++;
             }
-                if(s =="j") {
-                    b++;
-                }
-                    else(s== "n"){
-                        c++;
-                    }
-                }
+            if(s.charAt(i) == 'j'){
+                b++;
             }
+            else if(s.charAt(i) == 'n'){
+                c++;
+            }
+
         }
+        System.out.println("No. of words:" +"h" +a +"j" +b+"n"+ c);
     }
 }
